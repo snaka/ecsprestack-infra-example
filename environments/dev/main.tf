d0a17c1bf63b7d698e547ad04665fb477b82ed5a
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2023-08-29 22:12:27 +0900

    update

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
     tags = {
       Environment        = module.conf.env
       TerraformWorkspace = terraform.workspace
-      Test               = "test"
+      Test               = "test1"
     }
   }
 }
