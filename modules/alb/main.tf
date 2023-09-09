80fd9cafda01bf38bc71b5e838815f584f9428a1
S.Nakamatsu <19329+snaka@users.noreply.github.com>
2023-09-09 16:10:28 +0900

    change port 443 to 80

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "alb" {
 
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port = 443
-    to_port = 443
+    from_port = 80
+    to_port = 80
     protocol = "tcp"
   }
 
